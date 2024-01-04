8e783821b45b03aea319ce79f4b7df9b66d5cd2d
水上 皓登 <nainaistar@gmail.com>
2024-01-04 22:33:27 +0900

    メソッドだけ用意

--- src/diff.rs
+++ src/diff.rs
@@ -1,5 +1,6 @@
 extern crate serde_json;
 
+use std::collections::HashSet;
 use serde_json::Value;
 use std::fs::File;
 use std::io::Read;
@@ -27,6 +28,51 @@ pub fn diff() {
     }
 }
 
+
+pub struct I18nFile {
+    name: String,
+    keys: HashSet<String>,
+}
+
+pub struct I18nFileBuilder {
+    name: Option<String>,
+    keys: Option<HashSet<String>>,
+}
+
+impl I18nFileBuilder {
+    pub fn new() -> Self {
+        Self {
+            name: None,
+            keys: None,
+        }
+    }
+
+    pub fn name(mut self, name: String) -> Self {
+        self.name = Some(name);
+        self
+    }
+
+    pub fn keys(mut self, keys: HashSet<String>) -> Self {
+        self.keys = Some(keys);
+        self
+    }
+
+    pub fn build(self) -> Option<I18nFile> {
+        Some(I18nFile {
+            name: self.name?,
+            keys: self.keys?,
+        })
+    }
+}
+
+fn diff_key(one: I18nFile, two: I18nFile, args: Vec<I18nFile>) -> HashSet<String> {
+    for arg in args {
+        println!("{}", arg.name);
+    }
+
+    return HashSet::new();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,5 +80,43 @@ mod tests {
     #[test]
     fn it_adds_two() {
         diff();
+    }
+}
+
+
+#[cfg(test)]
+mod diff_key {
+    use super::*;
+
+    #[test]
+    fn normal() {
+        let expected = HashSet::new();
+
+        let one = I18nFileBuilder::new()
+            .name("ja.json".to_string())
+            .keys(to_hash_set([
+                "Parent.Child.GrandChild.01",
+                "Parent.Child.GrandChild.02",
+                "GrandParent",
+            ]))
+            .build();
+
+        let two = I18nFileBuilder::new()
+            .name("ja.json".to_string())
+            .keys(to_hash_set([
+                "Parent.Child.GrandChild.01",
+                "Parent.Child.GrandChild.02",
+                "GrandParent",
+            ])).build();
+
+        let actual = diff_key(one.unwrap(), two.unwrap(), vec![]);
+        assert_eq!(actual, expected);
+    }
+
+    fn to_hash_set(param: [&str; 3]) -> HashSet<String> {
+        return param
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
     }
 }
